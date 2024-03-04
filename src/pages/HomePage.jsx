9ef7eb6b825a212327c9e98840795ed745afa127
Hamza <144457566+unk-user@users.noreply.github.com>
2024-03-04 20:41:49 +0100

    initial homepage layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,12 @@
-export default function HomePage () {
-  return <p>Home Page</p>
+import BrandSlider from '../components/BrandSlider';
+import HomeProduct from '../components/HomeProduct';
+
+export default function HomePage() {
+  return (
+    <main>
+      <h1 className='font-semibold text-7xl text-center my-16'>Top Brands</h1>
+      <BrandSlider />
+      <HomeProduct />
+    </main>
+  );
 }
