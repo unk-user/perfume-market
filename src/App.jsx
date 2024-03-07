653ab54b7fa43c142fa7633de2f0b87c289613f8
Hamza <144457566+unk-user@users.noreply.github.com>
2024-03-07 14:38:11 +0100

    add scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,19 @@ import NavHeader from './components/Navbar/NavHeader';
 import Footer from './components/Footer/Footer';
 import HomePage from './pages/HomePage';
 import ShopPage from './pages/ShopPage';
+import ProductPage from './pages/ProductPage';
+import ScrollToTop from './scrollToTop';
 
 const App = () => {
   return (
     <Router>
+      <ScrollToTop />
       <div className="w-full max-md:w-screen relative flex-col flex min-h-screen">
         <NavHeader />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="shop" element={<ShopPage />} />
+          <Route path='shop/products/:productId' element={<ProductPage />}/>
         </Routes>
         <Footer />
       </div>
