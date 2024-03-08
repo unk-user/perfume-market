31310ebb2471d80ebab64921bba2bf30ec320b1d
Hamza <144457566+unk-user@users.noreply.github.com>
2024-03-08 10:41:28 +0100

    add MoreProducts section

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,11 +1,15 @@
 import { useParams } from 'react-router-dom';
-import ProductDetails from '../components/ProductPage/ProductDetails';
+import ProductDetails from '../components/Product/ProductDetails';
 import data from '../data/dataset.json';
-import ProductPreview from '../components/ProductPage/ProductPreview';
+import ProductPreview from '../components/Product/ProductPreview';
+import Products from '../components/Shop/Products';
 
 export default function ProductPage() {
   const { productId } = useParams();
   let product = data.find((item) => item.id == productId);
+  const products = data.filter((item) => {
+    return item.id != productId && item.category === product.category;
+  });
 
   if (!product) {
     return (
@@ -17,14 +21,15 @@ export default function ProductPage() {
 
   return (
     <main className="px-12">
-      <div className="flex justify-center py-12">
+      <div className="flex justify-center py-16">
         <div className="flex flex-col gap-2 lg:flex-row">
           <ProductPreview product={product} />
           <ProductDetails product={product} />
         </div>
       </div>
-      <div>
-        <p>More Products</p>
+      <div className='lg:px-32'>
+        <h1 className='text-xl'>More Products</h1>
+        <Products products={products}/>
       </div>
     </main>
   );
